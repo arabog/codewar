10befe38c054b997dc6a4132dffe37a2c84029eb
iCodeRx <arababs2016@gmail.com>
2021-03-12 13:50:24 +0100

    Basic data types--Boolean and conditional

---
+++ firstDay/boolCon.js
@@ -0,0 +1,15 @@
+/* the Boolean value is mainly used for conditional judgment.
+
+Coding in function trueOrFalse, function accept 1 parameters:val, 
+try to use the conditional statement if...else, if val value is false 
+(val==false or it can convert to false), should return a string "false", 
+if not, return a string "true".
+*/
+// function trueOrFalse(val){
+//           if ()    return ?;             
+//           else     return ?;
+// }
+
+function trueOrFalse(val){
+          return val ? "true" : "false";
+}
